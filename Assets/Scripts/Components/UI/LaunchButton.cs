3d53a98097b25cae1ba114b0747a369337ebcdf2
Excilyano <agahnim13@gmail.com>
2019-10-05 21:23:06 +0200

    UI complete

---
+++ Assets/Scripts/Components/UI/LaunchButton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LaunchButton : MonoBehaviour
+{
+    public GameObject content;
+
+    public void ToGame() {
+        foreach(Button button in content.GetComponentsInChildren<Button>(true)) {
+            button.gameObject.SetActive(!button.gameObject.activeInHierarchy);
+        }
+    }
+}
