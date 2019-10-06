bf6f06fc599c225c65afb91ed53fb535d86d0027
Excilyano <agahnim13@gmail.com>
2019-10-06 14:44:37 +0200

    ajout par 5

--- Assets/Scripts/Components/UI/ShopController.cs
+++ Assets/Scripts/Components/UI/ShopController.cs
@@ -1,17 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShopController : MonoBehaviour
 {
     public UnitAsset unitAsset;
+
     public void Buy() {
         Unit unit = new Unit(unitAsset, new Vector3(0, 0, 0));
         ShopManager.PurchaseUnit(unit);
     }
 
+    public void Buy5() {
+        for (int i = 0; i < 5; i++) Buy();
+    }
+
     public void Sell() {
         Unit unit = new Unit(unitAsset, new Vector3(0, 0, 0));
         ShopManager.SellUnit(unit);
+    }
+
+    public void Sell5() {
+        for (int i = 0; i < 5; i++) Sell();
     }
 }
